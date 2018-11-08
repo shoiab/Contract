ba937881f863848bb97c4f82bf82e8d608bea78c
Shoiab Khan <shoiab.khan@myntra.com>
2018-11-09 04:51:33 +0530

    removed unwanted reports code

--- src/main/java/com/brokerage/controller/ContractController.java
+++ src/main/java/com/brokerage/controller/ContractController.java
@@ -95,25 +95,6 @@ public class ContractController {
         return contractService.getRoles();
     }
 
-
-    /*@RequestMapping(value = "/pdfreports", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<InputStreamResource> pdfReports() throws IOException {
-
-        List<Contract> contracts = contractService.getAll();
-
-        ByteArrayInputStream bis = GeneratePdfReport.contractReport(contracts);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Disposition", "inline; filename=contracts.pdf");
-
-        return ResponseEntity
-                .ok()
-                .headers(headers)
-                .contentType(MediaType.APPLICATION_PDF)
-                .body(new InputStreamResource(bis));
-    }*/
-
     @RequestMapping(value = "/pdfreport", method = RequestMethod.GET,
             produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<InputStreamResource> pdfReport(@RequestParam(value = "id") long id) throws IOException {
